5f1904675d8b38833b6e586f816f3533bfa5bf2d
CHIRAG DALMIA <119760552+ChiragDalmia@users.noreply.github.com>
2023-05-23 13:54:22 -0400

    Update1

--- src/softdesign/SoftDesign.java
+++ src/softdesign/SoftDesign.java
@@ -3,14 +3,14 @@ package softdesign;
 public class SoftDesign {
     public static void main(String[] args) {
         // Create an array of Student objects
-        Student[] students = new Student[5];
+        Student[] students = new Student[4];
 
         // Populate the array with classmates' names
         students[0] = new Student("John", 1001);
         students[1] = new Student("Alice", 1002);
         students[2] = new Student("Bob", 1003);
         students[3] = new Student("Emily", 1004);
-        students[4] = new Student("David", 1005);
+        
 
         // Iterate over the array and print each student's name
         for (Student student : students) {
